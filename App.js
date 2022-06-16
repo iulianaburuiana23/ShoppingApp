ad5dc9f20b2ecc057bafb19957dabf904a0d0ea9
Buruiana Iuliana <buruiana.iuliana.maria@gmail.com>
2022-06-16 17:03:00 +0300

    second commit

--- App.js
+++ App.js
@@ -6,6 +6,8 @@ import{createNativeStackNavigator}  from "@react-navigation/native-stack";
 import Home from './components/screens/Home.js';
 import MyCart from './components/screens/MyCart.js';
 import ProductInfo from './components/screens/ProductInfo.js';
+import LoginScreen from './components/screens/LoginScreen.js';
+import RegisterScreen from './components/screens/RegisterScreen.js';
 const App=() =>{
   const Stack= createNativeStackNavigator();
 
@@ -19,6 +21,8 @@ const App=() =>{
         <Stack.Screen name="Home" component= {Home}/>
         <Stack.Screen name="MyCart" component= {MyCart}/>
         <Stack.Screen name="ProductInfo" component= {ProductInfo}/>
+        <Stack.Screen name="Register" component={RegisterScreen}/>
+        <Stack.Screen name="Login" component={LoginScreen}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
